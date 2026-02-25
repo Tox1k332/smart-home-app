784de8b3fb4b47a6a6d74878aa0b79554e1bdab0
Данил Косолапов <den.pvkp@mail.ru>
2026-02-25 15:12:09 +0600

    Fix router 404 on page refresh - use hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const routes = [
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/smart-home-app/'),
+  history: createWebHashHistory('/smart-home-app/'),
   routes
 })
 
